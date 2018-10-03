17667120fd3fe584814a399f524d4368a41e724f
harshit verma <harshit8858@gmail.com>
2018-10-03 14:48:50 +0530

    minor changes

--- django_pdf/settings.py
+++ django_pdf/settings.py
@@ -34,10 +34,10 @@ ALLOWED_HOSTS = []
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'harshit8858@gmail.com'
-EMAIL_HOST_PASSWORD = 'iamharshit8858'
+EMAIL_HOST_USER = '#######'
+EMAIL_HOST_PASSWORD = '########'
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'harshit8858@gmail.com'
+DEFAULT_FROM_EMAIL = '#######'
 
 
 # DROPBOX_APP_KEY = "ceeazamyfgh1xnr"
